370b93620842042a033dfd7fbd2189dbdd26a1e8
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-25 16:12:44 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
 def pascal_triangle(n):
+    """ creates a pascal triangle """
     if n <= 0:
-        return
+        return []
+    triangle = [[1]]
+    for r in range(1,n):
+        new_row = [1]
+        for i in range(1,r):
+            mid_raw = new_row.append(triangle[r-1][i-1] + triangle[r-1][i])
+        end_raw = new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
