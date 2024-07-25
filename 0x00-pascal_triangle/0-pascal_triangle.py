da4542e69d370032da97c2ac13322f4c55a4f235
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-07-25 16:16:03 +0100

    fixing whitespace

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,9 +9,9 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = [[1]]
-    for r in range(1,n):
+    for r in range(1, n):
         new_row = [1]
-        for i in range(1,r):
+        for i in range(1, r):
             mid_raw = new_row.append(triangle[r-1][i-1] + triangle[r-1][i])
         end_raw = new_row.append(1)
         triangle.append(new_row)
