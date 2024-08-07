f7885dd25044ae7e452d1fe303f985329db8fe2b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-07 14:39:37 +0100

    lets check now

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
 from typing import List
-"""Lockboxes"""
 
 
-def canUnlockAll(boxes):
-    """Lockboxes"""
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+    boxes (List[List[int]]):
+    A list of lists, where each sublist contains keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, otherwise False.
+
+    Steps:
+    1. Determine the number of boxes (n).
+    2. Create a set to keep track of which boxes have been opened (opened).
+    3. Start processing with the first box (box 0),
+        which is initially unlocked.
+    4. Process each box in the queue (to_process):
+        a. Get a box from the queue.
+        b. If the box has not been opened yet:
+            i. Mark the box as opened.
+            ii. Check all keys in the current box.
+            iii. For each key, if it corresponds to a valid box,
+                add the box to the queue.
+    5. Check if all boxes have been opened.
+    6. Return True if all boxes have been opened, otherwise return False.
+    """
+
     n: int = len(boxes)
-    # Determine the number of boxes
     opened: set = set()
-    # Create a set to keep track of which boxes have been opened
     to_process: List[int] = [0]
-    # Start processing with the first box (box 0), which is initially unlocked
 
-    # Process each box in the queue
     while to_process:
-        current_box: int = to_process.pop()  # Get a box from the queue
+        current_box: int = to_process.pop()
 
-        if current_box not in opened:  # If the box has not been opened yet
-            opened.add(current_box)  # Mark the box as opened
+        if current_box not in opened:
+            opened.add(current_box)
 
-            # Check all keys in the current box
             for key in boxes[current_box]:
-                if key < n:  # Ensure the key corresponds to a valid box
+                if key < n:
                     to_process.append(key)
-                    # Add the box to the queue
 
-    # Check if all boxes have been opened
     return len(opened) == n
-# Return True if all boxes have been opened, otherwise False
