12dcbfd11d168abe1e1b81afaf8ee4264114816d
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-19 17:47:23 +0100

    checking the efficiency fail checkers

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Making Change"""
+from collections import deque
 
 
 def makeChange(coins, total):
@@ -10,12 +11,18 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    queue = deque([(0, 0)])  # (current amount, number of coins)
+    visited = set()  # to track visited amounts
 
-    for coin in coins:
-        for amt in range(coin, total + 1):
-            if dp[amt - coin] != float('inf'):
-                dp[amt] = min(dp[amt], dp[amt - coin] + 1)
+    while queue:
+        current_amount, num_coins = queue.popleft()
 
-    return dp[total] if dp[total] != float('inf') else -1
+        for coin in coins:
+            new_amount = current_amount + coin
+            if new_amount == total:
+                return num_coins + 1
+            if new_amount < total and new_amount not in visited:
+                visited.add(new_amount)
+                queue.append((new_amount, num_coins + 1))
+
+    return -1
